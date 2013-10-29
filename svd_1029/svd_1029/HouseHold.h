c222a2a9dde15cb23d39e39b061875bd3184732c
clarkwangbc <123687502@qq.com>
2013-10-29 20:23:11 +0800

    +1

--- svd_1029/svd_1029/HouseHold.h
+++ svd_1029/svd_1029/HouseHold.h
@@ -9,7 +9,10 @@ private:
 public:
 	HouseHold();//构造函数
 	HouseHold(int n);//构造n*n的方阵的household
+	
 	double Delta();//返回delta
+	Matrix * TMatrix();//返回函数指针
+	
 	void HouseHolder(Vector v);
 };
 #endif // ! HOUSEHOLD_H
